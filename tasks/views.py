3ce6ae5cf7c8ca91417fd35bc69b9b6f8222c4d7
ldxsoria <ldxsoria@gmail.com>
2022-10-25 22:27:05 -0500

    2:08:45 - Empezando a listar tareas completas

--- tasks/views.py
+++ tasks/views.py
@@ -48,6 +48,12 @@ def tasks(request):
         'tasks': tasks
     })
 
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
+
 def create_task(request):
     if request.method == 'GET':
         return render(request, 'create_task.html',{
